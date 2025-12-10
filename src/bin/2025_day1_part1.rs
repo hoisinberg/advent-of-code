f5681f675950ce89e2e987026cd97e662c5368b9
Michael <michaelq94@gmail.com>
2025-12-09 21:47:02 -0800

    Add greeting function

---
+++ src/bin/2025_day1_part1.rs
@@ -0,0 +1,5 @@
+use aoc::hoi::console::greet;
+
+fn main() {
+    greet(2025, 1, 1);
+}
---
+++ src/hoi/console.rs
@@ -0,0 +1,3 @@
+pub fn greet(year: u32, day: u32, part: u32) {
+    println!("Advent of Code {year}, day {day}, part {part}");
+}

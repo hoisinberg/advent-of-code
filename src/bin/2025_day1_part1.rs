b154d9281c10c6304785bb02c0f5c4a7147a12d1
Michael <michaelq94@gmail.com>
2025-12-09 23:42:41 -0800

    Day 1, part 1 solution

--- src/bin/2025_day1_part1.rs
+++ src/bin/2025_day1_part1.rs
@@ -1,11 +1,39 @@
 use aoc::hoi;
+use std::io;
+
+fn parse_rotation_sign(rotation: u8) -> io::Result<i32> {
+    match rotation {
+        b'R' => Ok(1),
+        b'L' => Ok(-1),
+        _ => Err(io::Error::new(
+            io::ErrorKind::InvalidData,
+            format!("Unable to parse rotation type {rotation}"),
+        )),
+    }
+}
+
+fn parse_rotation(str: String) -> io::Result<i32> {
+    let sign = parse_rotation_sign(str.as_bytes()[0])?;
+    let distance = &str[1..].parse().map_err(|parse_error| {
+        io::Error::new(
+            io::ErrorKind::InvalidData,
+            format!("Unable to parse rotation distance: {parse_error}"),
+        )
+    })?;
+    Ok(sign * distance)
+}
+
+fn rotate(dial_and_zero_count: (i32, u32), rotation: &i32) -> (i32, u32) {
+    let (dial, zero_count) = dial_and_zero_count;
+    let new_dial = (dial + rotation).rem_euclid(100);
+    let zero_count_delta = if new_dial == 0 { 1 } else { 0 };
+    (new_dial, zero_count + zero_count_delta)
+}
 
 fn main() {
     hoi::console::greet(2025, 1, 1);
-
-    let lines = hoi::file::read_lines("input/2025_day01.txt", &hoi::func::id_result)
-        .expect("Couldn't read file.");
-    for line in lines {
-        println!("{line}");
-    }
+    let rotations = hoi::file::read_lines("input/2025_day01.txt", &parse_rotation)
+        .expect("Failed to read and parse rotations.");
+    let (_, part1_zero_count) = rotations.iter().fold((50, 0), &rotate);
+    println!("Part 1 answer: {part1_zero_count}");
 }
